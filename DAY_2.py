9c85c3a2f406fc856c2c900b9973436fc873da4b
Aarush336 <aarushupadhyay614@gmail.com>
2026-03-09 17:55:24 +0530

    Introduction to Numpy Library

--- DAY_2.py
+++ DAY_2.py
@@ -1,12 +1,6 @@
-def check_water(plants):
-    if plants ["temperature"] > 39:
-        return("Needs water")
-    else:
-        return("Normal")
-plants = [
-    {"name": "Cactus", "temperature": 38, "water_needed": False},
-    {"name": "Rose", "temperature": 42, "water_needed": True},
-    {"name": "Sunflower", "temperature": 35, "water_needed": False}
-]
-for breed in plants:
-    print(breed["name"] , breed["temperature"], check_water(breed))
+# Introduction to Panda's and Numpy's
+import pandas as pd
+import numpy as np
+
+temperatures = np.array([38, 42, 35, 40, 33, 41, 37])
+print(temperatures[temperatures > 39])
