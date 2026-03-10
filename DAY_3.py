acc206c9f8be3e2fa284d5cad23f7cd7c140e7a9
Aarush336 <aarushupadhyay614@gmail.com>
2026-03-10 09:59:03 +0530

    Practise Question 3: Loops

--- DAY_3.py
+++ DAY_3.py
@@ -1,5 +1,7 @@
-# Practise Question 2: Create a list of 5 plant names. Print the second one. Print the last one using negative indexing. Print how many plants are in the list.
-plants = ["Cactus", "Sunflower", "Daisy", "Tulip", "Dandelion"]
-print(len(plants))
-print(plants[1])
-print(plants[-1])
+# Practise Question 3: Create a list of humidity readings: [55, 70, 80, 45, 90, 60, 75]. Loop through them and print "High humidity" if above 70, "Normal" if 70 or below.
+humidity_readings = [55, 70, 80, 45, 90, 60, 75]
+for hum in humidity_readings:
+    if hum > 70:
+        print("High Humidity")
+    else:
+        print("Normal")
