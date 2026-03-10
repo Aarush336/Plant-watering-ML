d090b62b4dffc3a7d1caa3eaa0f1aa158c86ed4f
Aarush336 <aarushupadhyay614@gmail.com>
2026-03-10 09:42:15 +0530

    Practise Question 2: Lists

--- DAY_3.py
+++ DAY_3.py
@@ -1,11 +1,5 @@
-# Practise question 1: Create 4 variables representing a plant's data — its name, its age in days, its current temperature, and whether it's alive or not. Print all four. 
-
-name = "Cactus"
-age = 37
-temperature = 41
-is_it_alive = True
-
-print ("Name:- " +name)
-print("Age:- " ,age)
-print("Current temperature:- " ,temperature)
-print("Is it alive:- " ,is_it_alive)
+# Practise Question 2: Create a list of 5 plant names. Print the second one. Print the last one using negative indexing. Print how many plants are in the list.
+plants = ["Cactus", "Sunflower", "Daisy", "Tulip", "Dandelion"]
+print(len(plants))
+print(plants[1])
+print(plants[-1])
